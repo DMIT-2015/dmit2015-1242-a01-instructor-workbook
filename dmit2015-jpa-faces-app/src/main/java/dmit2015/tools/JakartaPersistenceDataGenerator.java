a89f2c86ba4c60c1cb1af46d0a80bf93969c1f83
Sam Wu <swu@nait.ca>
2025-02-10 12:08:11 -0700

    End of Feb 10 class.

---
+++ dmit2015-jpa-faces-app/src/main/java/dmit2015/tools/JakartaPersistenceDataGenerator.java
@@ -0,0 +1,36 @@
+package dmit2015.tools;
+
+import dmit2015.model.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import net.datafaker.Faker;
+
+public class JakartaPersistenceDataGenerator {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.hibernate.jpa.HibernatePersistenceProvider");
+        // The persistenceUnitName can be passed as an argument otherwise it defaults to "local-mssql-dmit2015-jpa-pu"
+        String persistenceUnitName = (args.length == 1) ? args[0] : "resource-local-postgresql-jpa-pu";
+        // https://jakarta.ee/specifications/persistence/3.1/jakarta-persistence-spec-3.1.html#obtaining-an-entity-manager-factory-in-a-java-se-environment
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        JakartaPersistenceDataGenerator.generateData(em);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+        System.out.printf("Finished creating database schema for persistence unit %s\n", persistenceUnitName);
+    }
+
+    public static void generateData(EntityManager em) {
+        var faker = new Faker();
+        for(int counter = 1; counter <= 32; counter++) {
+            var currentStudent = new Student();
+            currentStudent.setFirstName(faker.name().firstName());
+            currentStudent.setLastName(faker.name().lastName());
+            currentStudent.setCourseSection("DMIT2015-A01");
+            em.persist(currentStudent);
+        }
+    }
+}
---
+++ dmit2015-jpa-faces-app/src/main/java/dmit2015/tools/JakartaPersistenceDatabaseSchemaGenerator.java
@@ -0,0 +1,48 @@
+package dmit2015.tools;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This program generates the database schema for a persistence unit name
+ * defined in `persistence.xml`with a RESOURCE_LOCAL transaction type.
+ * <p>
+ * The following is an example of a RESOURCE_LOCAL persistence unit:
+ *
+ * <pre>{@code
+ *     <persistence-unit name="resource-local-mssql-dmit2015-jpa-pu" transaction-type="RESOURCE_LOCAL">
+ *      <properties>
+ *             <property name="hibernate.dialect" value="org.hibernate.dialect.SQLServerDialect"/>
+ *             <property name="jakarta.persistence.jdbc.driver" value="com.microsoft.sqlserver.jdbc.SQLServerDriver" />
+ *             <property name="jakarta.persistence.jdbc.url" value="jdbc:sqlserver://localhost;databaseName=DMIT2015_CourseDB;TrustServerCertificate=true" />
+ *             <property name="jakarta.persistence.jdbc.user" value="user2015" />
+ *             <property name="jakarta.persistence.jdbc.password" value="Password2015" />
+ *     </persistence-unit>
+ * }
+ * </pre>
+ *
+ * @author Sam Wu
+ */
+public class JakartaPersistenceDatabaseSchemaGenerator {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.hibernate.jpa.HibernatePersistenceProvider");
+        // The persistenceUnitName can be passed as an argument otherwise it defaults to "local-mssql-dmit2015-jpa-pu"
+        String persistenceUnitName = (args.length == 1) ? args[0] : "resource-local-mssql-dmit2015-jpa-pu";
+        // Create a map that defines properties to control database schema generation.
+        Map<String, String> properties = new HashMap<>();
+        // Set the map property to drop all mapped database tables then re-create the mapped database tables..
+        // Valid value for jakarta.persistence.schema-generation.database.action are:  "none", "create", "drop-and-create", "drop"
+
+        properties.put("jakarta.persistence.schema-generation.database.action", "drop-and-create");
+        // https://jakarta.ee/specifications/persistence/3.1/jakarta-persistence-spec-3.1.html#obtaining-an-entity-manager-factory-in-a-java-se-environment
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName, properties);
+        emf.createEntityManager().close();
+        emf.close();
+        System.out.printf("Finished creating database schema for persistence unit %s\n", persistenceUnitName);
+    }
+
+}
