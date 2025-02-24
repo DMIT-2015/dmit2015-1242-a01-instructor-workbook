50156161460d2ab03f846d18af3a4a165a326744
Sam Wu <swu@nait.ca>
2025-02-24 11:56:08 -0700

    Fixed strict JPQL compliance warning

--- dmit2015-oracle-hr-app/src/main/java/dmit2015/repository/EmployeeRepository.java
+++ dmit2015-oracle-hr-app/src/main/java/dmit2015/repository/EmployeeRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository {
 
-    @Query("from Department d where lower(d.departmentName) like lower(?1) order by d.departmentName")
+    @Query("select d from Department d where lower(d.departmentName) like lower(?1) order by d.departmentName")
     List<Department> departmentsBy(String departmentName);
 
-    @Query("from Employee e join fetch e.department join fetch e.job join fetch e.manager where e.department.id = ?1")
+    @Query("select e from Employee e join fetch e.department join fetch e.job join fetch e.manager where e.department.id = ?1")
     List<Employee> employeesByDepartmentId(Short id);
 
     @Find
